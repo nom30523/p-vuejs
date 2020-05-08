86c8ca1e352ae4de41000cd2db890f6d3cf64852
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-08 19:49:23 +0900

    ローカルコンポーネント練習

--- p-vuejs/src/App.vue
+++ p-vuejs/src/App.vue
@@ -1,3 +1,16 @@
 <template>
-  <LikeNumber></LikeNumber>
+  <div>
+    <LikeHeader></LikeHeader>
+    <LikeNumber></LikeNumber>
+  </div>
 </template>
+
+<script>
+import LikeHeader from "./components/LikeHeader.vue";
+
+export default {
+  components: {
+    LikeHeader
+  }
+}
+</script>
