885706a7f924348c939bbfaa114bf1917b43e92e
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-08 19:35:59 +0900

    グローバルコンポーネント練習

---
+++ p-vuejs/src/LikeNumber.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>いいね({{ number }})</p>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      number: 5
+    }
+  },
+  methods: {
+    increment: function() {
+      this.number += 1
+    }
+  }
+}
+</script>
+
+<style>
+</style>
