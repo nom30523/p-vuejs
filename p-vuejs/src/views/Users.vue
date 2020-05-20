51cb8c06c865f62947b8d87c6d0db9bb4a537b92
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-20 15:48:24 +0900

    v-bindでURLのidを動的にする

--- p-vuejs/src/views/Users.vue
+++ p-vuejs/src/views/Users.vue
@@ -3,6 +3,7 @@
     <h3>Users</h3>
     <hr>
     <h1>User No. {{ id }}</h1>
+    <router-link :to="'/users/' + (Number(id) + 1) + '/profile'">次のユーザー</router-link>
     <router-view></router-view>
   </div>
 </template>
