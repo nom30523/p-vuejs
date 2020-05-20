5d86f52ccc30dda00a08aab2839b64c8ab8c81c2
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-20 17:00:14 +0900

    名前付きビュー

---
+++ p-vuejs/src/views/HeaderHome.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav>
+    <h2>Home</h2>
+    <router-link to="/" active-class="link--active" exact class="link">Home</router-link>
+    <router-link to="/users" active-class="link--active" exact class="link">Users</router-link>
+  </nav>
+</template>
