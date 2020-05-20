7ca855f9deb37f2eeb1210b9a092f92a42e2a5b6
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-20 14:21:56 +0900

    vuerouter 導入

--- p-vuejs/src/main.js
+++ p-vuejs/src/main.js
@@ -1,18 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import LikeNumber from "./components/LikeNumber.vue"
+import router from './router'
 
 Vue.config.productionTip = false
-// Vue.component('LikeNumber', LikeNumber);
-// Vue.filter("upperCase", function(value) {
-//   return value.toUpperCase();
-// });
-// Vue.mixin({
-//   created() {
-//     console.log("global mixin");
-//   }
-// });
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
